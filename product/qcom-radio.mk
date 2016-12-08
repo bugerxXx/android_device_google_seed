a41536aa7bda74dbd93e5c70b1e64f92dd43c8ae
bugerxXx <muhammadiqbal20122012@gmail.com>
2016-12-08 23:20:02 +0700

    Try to Fix RIL

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -8,10 +8,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.default_network=9,9
 
 # RIL
-PRODUCT_PACKAGES += \
-    libxml2
-
-# RIL
 ifeq ($(QCPATH),)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/data/netmgr_config.xml:system/etc/data/netmgr_config.xml \
