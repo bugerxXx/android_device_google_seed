acb985dc42336f3c8f47fcd6da4071bab527a748
bugerxXx <muhammadiqbal20122012@gmail.com>
2016-12-23 13:55:59 +0700

    wt86518: Cleanup tree, use init_msm

--- board/kernel.mk
+++ board/kernel.mk
@@ -4,8 +4,6 @@ BOARD_RAMDISK_OFFSET     := 0x01000000
 TARGET_KERNEL_SOURCE := kernel/lenovo/wt86518
 TARGET_KERNEL_CONFIG := cyanogenmod_wt86518_defconfig
 BOARD_KERNEL_SEPARATED_DT := true
-TARGET_KERNEL_APPEND_DTB := true
-TARGET_USES_UNCOMPRESSED_KERNEL := true
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 2048
